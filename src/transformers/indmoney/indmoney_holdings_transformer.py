73c13f30d6f885667bd41fb78c7f751c223088b8
mallikarjunPC <mallikarjunahi@gmail.com>
2026-04-10 20:09:18 +0530

    indmoney holdings transformer is ready

--- src/transformers/indmoney/indmoney_holdings_transformer.py
+++ src/transformers/indmoney/indmoney_holdings_transformer.py
@@ -13,33 +13,44 @@ from src.transformers.helper import (
 )
 
 COLUMNS_TO_DROP_FROM_PARSER: Final = [
-    "account_type",
-    "description",
+    "Description",
 ]
 
 DATE_FORMAT_FOR_FRANKFURTER_API: Final = "%Y-%m-%d"
+
+COLUMNS_TO_RENAME: Final = {
+    "Symbol":"symbol",
+    "Quantity":"quantity",
+    "Market Price":"market_price",
+    "Market Value":"market_value",
+    "Cost Price":"unit_cost",
+    "Unrealized":"gain",
+    "TD Cost Basis":"total_cost",
+}
+
+SYMBOL_ROW_TO_DROP: Final = "*Cash"
 
 # =========================
 # Main transformer
 # =========================
 def transform_indmoney_holdings(
-        vest_holdings_extract_df: pd.DataFrame,
+        indmoney_holdings_extract_df: pd.DataFrame,
         month_year: str,
 ) -> pd.DataFrame:
     """
-    Transform raw vest holdings into schema that is helpful to determine invested and current value in INR    
+    Transform raw indmoney holdings into schema that is helpful to determine invested and current value in INR    
     
     Parameters
     ----------
-    vest_holdings_extract_df: pd.DataFrame
-        Output dataframe from vest_holdings_parser
+    indmoney_holdings_extract_df: pd.DataFrame
+        Output dataframe from indmoney_holdings_parser
     month_year : str
-        The period for which this vest statement generated
+        The period for which this indmoney statement generated
     
     Returns
     -------
     pd.DataFrame
-        Final transformed dataframe holding the vest statement period snapshot (invested & current computable in INR)
+        Final transformed dataframe holding the indmoney statement period snapshot (invested in USD & current computable in INR)
     """
     if not month_year:
         logger.error(
@@ -47,42 +58,46 @@ def transform_indmoney_holdings(
         )
         raise ValueError("month_year must be provided in MM/YYYY format")
     
-    logger.info("transforming vest holdings for %s",month_year)
+    logger.info("transforming indmoney holdings for %s",month_year)
     # ----------------------------------
-    # considering interested columns
+    # reanming columns and considering interested columns and rows
     # ----------------------------------
-    vest_holdings_transformed_df = (
-        vest_holdings_extract_df
+    indmoney_holdings_transformed_df = (
+        indmoney_holdings_extract_df
         .drop(columns=COLUMNS_TO_DROP_FROM_PARSER)
+        .rename(columns=COLUMNS_TO_RENAME)
     )
+
+    mask = indmoney_holdings_transformed_df["symbol"] != SYMBOL_ROW_TO_DROP
+    indmoney_holdings_transformed_df = indmoney_holdings_transformed_df[mask]
 
     # -----------------------------------------
     # cleaning and numerical format conversion
     # -----------------------------------------
     columns_to_clean_convert = [
-        col for col in vest_holdings_transformed_df.columns if col!="symbol"
+        col for col in indmoney_holdings_transformed_df.columns if col!="symbol"
     ]
 
-    vest_holdings_transformed_df[columns_to_clean_convert] = (
-        vest_holdings_transformed_df[columns_to_clean_convert]
+    indmoney_holdings_transformed_df[columns_to_clean_convert] = (
+        indmoney_holdings_transformed_df[columns_to_clean_convert]
         .apply(_clean_convert_currency_column_to_numeric)
     )
 
     # -----------------------------------------
     # inserting month end date column
     # -----------------------------------------
-    vest_holding_date = _derive_month_end_date_for_postgres_posting(month_year)
+    indmoney_holding_date = _derive_month_end_date_for_postgres_posting(month_year)
 
-    logger.info("inserting %s as date column for vest holdings period %s",vest_holding_date,month_year)
+    logger.info("inserting %s as date column for vest holdings period %s",indmoney_holding_date,month_year)
 
-    vest_holdings_transformed_df["date"] = vest_holding_date
+    indmoney_holdings_transformed_df["date"] = indmoney_holding_date
 
     # ---------------------------------------------------------------------
     # fetching usd to inr exchange rate from API and inserting as column
     # ---------------------------------------------------------------------
     # build the helper function for _fetch_usd_to_inr_exch_rate_from_Frankfurter_API
-    usd_to_inr_exch_rate = _fetch_usd_to_inr_exch_rate_from_Frankfurter_API(vest_holding_date.strftime(DATE_FORMAT_FOR_FRANKFURTER_API))
+    usd_to_inr_exch_rate = _fetch_usd_to_inr_exch_rate_from_Frankfurter_API(indmoney_holding_date.strftime(DATE_FORMAT_FOR_FRANKFURTER_API))
 
-    vest_holdings_transformed_df["exch_rate"] = usd_to_inr_exch_rate
+    indmoney_holdings_transformed_df["exch_rate"] = usd_to_inr_exch_rate
 
-    return vest_holdings_transformed_df
+    return indmoney_holdings_transformed_df
